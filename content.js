62ae77359d1e8641a08edcf19a5d595ec73dcd2c
William Bruno <rocha_bruno@hotmail.com>
2016-03-20 16:24:23 -0300

    change: prices to 5 reais

--- content.js
+++ content.js
@@ -5,42 +5,42 @@ let products = {
     name: 'Empanada de Carne',
     description: 'A tradicional empanada de carne moída, típico argentina. Carne moída, cebola, bacon, orégano, azeitonas, uva passas e sal.',
     tpl: 'empanada-de-carne',
-    price: '4,00',
+    price: '5,00',
     url: '/empanada-de-carne.html'
   },
   carnePimenta: {
     name: 'Empanada de Carne com Pimenta',
     description: 'Todo o sabor da tradicional empanada típico argentina com pimenta do reino, páprica doce, cumim, salsinha e sal.',
     tpl: 'empanada-de-carne-com-pimenta',
-    price: '4,00',
+    price: '5,00',
     url: '/empanada-de-carne-com-pimenta.html'
   },
   frangoComCatupiry: {
     name: 'Empanada de Frango com Catupiry',
     description: 'Todo o sabor da tradicional empanada típico argentina no recheio com frango desfiado e requeijão da marca Catupiry.',
     tpl: 'empanada-de-frango-com-catupiry',
-    price: '4,00',
+    price: '5,00',
     url: '/empanada-de-frango-com-catupiry.html'
   },
   calabresa: {
     name: 'Empanada de Calabresa',
     description: 'Empanada de Calabresa, azeitonas, cebola, páprica doce, pimenta calabresa e erva doce.',
     tpl: 'empanada-de-calabresa',
-    price: '4,00',
+    price: '5,00',
     url: '/empanada-de-calabresa.html'
   },
   palmito: {
     name: 'Empanada de Palmito',
     description: 'Empanada de palmito selecionado e molho bechamel.',
     tpl: 'empanada-de-palmito',
-    price: '4,00',
+    price: '5,00',
     url: '/empanada-de-palmito.html'
   },
   bacalhau: {
     name: 'Empanada de Bacalhau',
     description: 'Empanada de bacalhau desfiado e purê de batatas.',
     tpl: 'empanada-de-bacalhau',
-    price: '6,00',
+    price: '5,00',
     url: '/empanada-de-bacalhau.html'
   }
 };
